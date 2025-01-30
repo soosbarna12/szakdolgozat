c81b16fa79693b05ebc83bb9f1210020be8e63ff
Soos Barna Istvan <soosbarna12@stud.uni-obuda.hu>
2025-01-30 18:19:38 +0100

    Project setup

---
+++ weather-warehouse/src/HomePage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to the Weather Warehouse</h1>
+      <p>Here you can find all the weather data you need</p>
+    </div>
+  );
+}
