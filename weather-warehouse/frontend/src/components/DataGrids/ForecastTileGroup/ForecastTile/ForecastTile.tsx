dccfb919970e7cc8b7a337eeb2c72b0260b93edf
Soos Barna Istvan <soosbarna12@stud.uni-obuda.hu>
2025-05-11 11:14:52 +0200

    Forecast tile styling

--- weather-warehouse/frontend/src/components/DataGrids/ForecastTileGroup/ForecastTile/ForecastTile.tsx
+++ weather-warehouse/frontend/src/components/DataGrids/ForecastTileGroup/ForecastTile/ForecastTile.tsx
@@ -24,47 +24,59 @@ export function ForecastTile({ data, isLoading }: Readonly<ForecastTileProps>) {
           padding: theme.spacing(2),
           backgroundColor: theme.palette.background.paper,
           borderRadius: "10px",
-          width: "100%",
-          height: "100%",
+          width: "250px",
+          height: "210px",
+          display: "flex",
+          flexDirection: "column",
+          gap: theme.spacing(1),
         }}
       >
-        <Skeleton variant="rectangular" animation="wave" width="150px" height="120px" sx={{ mb: 2 }} />
+        <Skeleton variant="rectangular" animation="wave" width="100%" height="100%" sx={{ borderRadius: "10px" }} />
       </StyledItem>
-    )
-  };
+    );
+  }
 
   function renderContent() {
     if (isLoading) return null;
 
-    return (<StyledItem
-      data-testid="forecastTile"
-      sx={{
-        textAlign: "left",
-        padding: theme.spacing(2),
-        backgroundColor: theme.palette.background.paper,
-        borderRadius: "10px",
-        width: "100%",
-        height: "100%",
-      }}
-    >
-      <Typography data-testid="dayName" variant="h6" fontWeight="700">
-        {getFormattedTime(date).dayName}
-      </Typography>
-      <Typography data-testid="date" variant="subtitle1">
-        {getFormattedTime(date).formattedDate}
-      </Typography>
-      <Box sx={{ display: "flex", alignItems: "center", mt: 1 }} color={theme.palette.primary.dark}>
-        <Typography data-testid="temperature" variant="h2" sx={{ fontSize: "30px", ml: 1, m: 0 }}>
-          {temperatureScaleChanger(TemperatureScale.Celsius, temperatureScale as TemperatureScale, Number(temperature != null ? temperature.toFixed(2) : "N/A"))}
-          <span style={{ fontSize: "0.5em", verticalAlign: "super" }}>{temperatureScaleLabel}</span>
+    return (
+      <StyledItem
+        data-testid="forecastTile"
+        sx={{
+          textAlign: "left",
+          padding: theme.spacing(2),
+          backgroundColor: theme.palette.background.paper,
+          borderRadius: "10px",
+          width: "250px",
+          height: "210px",
+          display: "flex",
+          flexDirection: "column",
+          gap: theme.spacing(1),
+        }}
+      >
+        <Typography data-testid="dayName" variant="h6" fontWeight="700">
+          {getFormattedTime(date).dayName}
         </Typography>
-      </Box>
-      <Box>
-        <Typography data-testid="precipitation">Precipitation: {precipitation?.toFixed(2) ?? 0} mm/h</Typography>
-        <Typography data-testid="pressure">Pressure: {pressure?.toFixed(2) ?? 0} hPa</Typography>
-      </Box>
-    </StyledItem>)
-  };
+        <Typography data-testid="date" variant="subtitle1">
+          {getFormattedTime(date).formattedDate}
+        </Typography>
+        <Box sx={{ display: "flex", alignItems: "center", mt: 1 }} color={theme.palette.primary.dark}>
+          <Typography data-testid="temperature" variant="h2" sx={{ fontSize: "30px", ml: 1, m: 0 }}>
+            {temperatureScaleChanger(
+              TemperatureScale.Celsius,
+              temperatureScale as TemperatureScale,
+              Number(temperature != null ? temperature.toFixed(2) : "N/A")
+            )}
+            <span style={{ fontSize: "0.5em", verticalAlign: "super" }}>{temperatureScaleLabel}</span>
+          </Typography>
+        </Box>
+        <Box>
+          <Typography data-testid="precipitation">Precipitation: {precipitation?.toFixed(2) ?? 0} mm/h</Typography>
+          <Typography data-testid="pressure">Pressure: {pressure?.toFixed(2) ?? 0} hPa</Typography>
+        </Box>
+      </StyledItem>
+    );
+  }
 
   function getFormattedTime(date: string) {
     const dayName = dayjs(date).format("dddd");
--- weather-warehouse/frontend/src/pages/ForecastPage/ForecastPage.tsx
+++ weather-warehouse/frontend/src/pages/ForecastPage/ForecastPage.tsx
@@ -23,9 +23,12 @@ export function ForecastPage() {
 
   function renderForecastTiles() {
     if (error) return null;
+
     return (forecastData ?? [1, 2, 3, 4, 5, 6, 7])?.map((data: ForecastData, index: number) => (
-      <Grid key={index} size={{ xs: 4, sm: 4, md: 4, lg: 3 }} sx={{ display: "flex", justifyContent: "center" }}>
-        <ForecastTile data={data} isLoading={isLoading} />
+      <Grid key={index} xs={4} sx={{ display: "flex", justifyContent: "center", }}>
+        <>
+          <ForecastTile data={data} isLoading={isLoading} />
+        </>
       </Grid>
     ));
   }
@@ -33,12 +36,26 @@ export function ForecastPage() {
   return (
     <>
       <FilterBar type={Pages.Forecast} location={location.name} />
-      <ContentBox sx={{ display: "flex", justifyContent: "center", paddingBottom: "2px" }}>
-        <Grid container spacing={2} justifyContent="center" alignItems="center">
+      <ContentBox
+        sx={{
+          paddingBottom: "2px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Grid
+          container
+          spacing={2}
+          sx={{
+            maxWidth: "1200px",
+            justifyContent: "center",
+          }}
+        >
           {renderError()}
           {renderForecastTiles()}
         </Grid>
       </ContentBox>
     </>
   );
-}
+};
